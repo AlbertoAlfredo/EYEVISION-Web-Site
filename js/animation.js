c1f78a5090d5b5faa003d6a69e25cfaa900d9099
AlbertoAlfredo <raioalfredo@gmail.com>
2021-01-07 19:41:23 -0300

    Alteraçoes de css, parte que vai para o teste de ia e iniciado projeto no wordpress para testes

--- js/animation.js
+++ js/animation.js
@@ -1,17 +1,17 @@
 const throttle = _.throttle;
 
 
-const players = 
+const players =
     Array.from(document.querySelectorAll('.js-play-on-screen'));
 
 
 function isOnScreen(el) {
-    let rect = el.getBoundingClientRect() 
+    let rect = el.getBoundingClientRect()
     return rect.top > 0 && rect.bottom < window.innerHeight;
 }
 
 function playAnimation(el) {
-    if(isOnScreen(el)) el.style.animationPlayState = 'running';
+    if (isOnScreen(el)) el.style.animationPlayState = 'running';
 }
 
 
@@ -20,3 +20,15 @@ const render = throttle(() => players.forEach(playAnimation), 150);
 render();
 window.addEventListener('scroll', render);
 
+
+/* Animação das imagens da galeria */
+$(".galeria-imagem").bind('mouseover', function () {
+
+    $(this).animate({ height: "200px", width: "200px" });
+
+});
+$(".galeria-imagem").bind('mouseout', function () {
+
+    $(this).animate({ height: "76px", width: "75px" });
+
+});
